0a480ad7f8dc7a1d2080ecd277c12ef195ab9846
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-14 21:35:49 +0600

    starter

---
+++ src/main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import Home from './components/Layout/Home'
+import Shop from './components/Shop/Shop'
+import Orders from './components/Orders/Orders'
+import Inventory from './components/Inventory/Inventory'
+import Login from './components/Login/Login'
+import cartProductsLoader from './assets/loaders/cartProductsLoader'
+import Checkout from './components/Checkout/Checkout'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/',
+        element:<Shop></Shop>
+      },
+      {
+        path:'orders',
+        element:<Orders></Orders>,
+        // loader: ()=> fetch('products.json')
+        loader: cartProductsLoader
+      },
+      {
+        path:'inventory',
+        element:<Inventory></Inventory>
+      },
+      {
+        path:'login',
+        element:<Login></Login>
+      },
+      {
+        path:'checkout',
+        element:<Checkout></Checkout>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
